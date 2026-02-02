b33bf8d44cccc5bda79f82ac9c9aa38e0952dbfd
chetankronad06 <chetankronad06@gmail.com>
2026-02-02 21:18:02 +0530

    Lower bound

---
+++ BinarySearch/lowerBound.java
@@ -0,0 +1,23 @@
+ class lowerBound {
+    public static int search(int[] nums, int target) {
+        int low=0;
+        int high = nums.length-1;
+		int ans = nums.length;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] >= target){
+				ans = mid;
+				high = mid-1;
+			}
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+	
+	public static void main(String[] args){
+		int arr[] = {1,2,2,3};
+		System.out.print(search(arr,2));
+	}
+}
